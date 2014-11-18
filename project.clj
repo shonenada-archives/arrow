d3c7c5ee454e79476ecb3ac718ff6766a15c7d70
shonenada <shonenada@gmail.com>
2014-11-19 00:43:26 +0800

    add dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/java.jdbc "0.3.5"]
+                 [org.clojure/tools.logging "0.3.0"]
                  [ring/ring-core "1.2.2"]
                  [ring/ring-jetty-adapter "1.2.2"]
-                 [compojure "1.0.1"]
                  [mysql/mysql-connector-java "5.1.31"]
+                 [environ "0.5.0"]
+                 [ring/ring-json "0.3.1"]
+                 [compojure "1.0.1"]
+                 [org.mindrot/jbcrypt "0.3m"]
                  [korma "0.4.0"]])
