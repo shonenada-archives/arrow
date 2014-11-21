11ac8af4f2a07a22e55dc861f7981a9acbb3c5cc
shonenada <shonenada@gmail.com>
2014-11-21 18:56:36 +0800

    fix config.clj, add default

--- src/arrow/config.clj
+++ src/arrow/config.clj
@@ -5,14 +5,15 @@
 (defonce web-host (env :arrow-web-host "0.0.0.0"))
 (defonce web-port (->int (env :arrow-web-port "3000")))
 (defonce secret-key (env :arrow-secret-key))
+(defonce cookie-key (env :arrow-cookie-key))
 
 ; database config
-(defonce db-type (env :arrow-db-type))
-(defonce db-host (env :arrow-db-host))
-(defonce db-port (env :arrow-db-port))
-(defonce db-name (env :arrow-db-name))
-(defonce db-username (env :arrow-db-username))
-(defonce db-password (env :arrow-db-password))
+(defonce db-type (env :arrow-db-type "mysql"))
+(defonce db-host (env :arrow-db-host "127.0.0.1"))
+(defonce db-port (env :arrow-db-port 3306))
+(defonce db-name (env :arrow-db-name "arrow"))
+(defonce db-username (env :arrow-db-username ""))
+(defonce db-password (env :arrow-db-password ""))
 
 (cond
   (= db-type "mysql") (defonce db-subname (str "//" db-host ":" db-port "/" db-name))
