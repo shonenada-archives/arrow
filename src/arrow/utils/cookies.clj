096ee3e092648adfb64b8ff94df3193a4567ac76
shonenada <shonenada@gmail.com>
2014-11-27 02:08:24 +0800

    fix bug in cookies

--- src/arrow/utils/cookies.clj
+++ src/arrow/utils/cookies.clj
@@ -7,32 +7,42 @@
    :path "/"
    :max-age config/cookie-age})
 
+(defn expired-cookie []
+  {:value (cipher/aes-encrypt "expired" config/cookie-key)
+   :path "/"
+   :max-age 0
+   :expires "Thu, 01 Jan 1970 00:00:00 GMT"})
+
 (defn gen-cookie [input]
   (-> input
       str
       (cipher/aes-encrypt config/cookie-key)
       ->cookie))
 
+(defn ->cookie-name [key-name]
+  (str config/cookie-prefix key-name))
+
 (defn get-cookie [req key-n]
   (let [key-name (name key-n)
-        cookie-name (str config/cookie-prefix key-name)
-        cookies (req :cookies)
-        cookie (-> cookies (get cookie-name) :value)]
-    (str (cipher/aes-decrypt cookie config/cookie-key))))
-
-(defn add-cookies [req resp key-n value]
-  (let [key-name (name key-n)
-        cookie-name (str config/cookie-prefix key-name)
-        cookie {cookie-name (gen-cookie value)}
-        cookies (req :cookies)]
-    (if-let [old-cookie (-> cookies (get cookie-name) :value)]
-      (dissoc cookies cookie-name))
-    (let [insert-cookies (conj cookies cookie)]
-      (assoc resp :cookies insert-cookies))))
-
-(defn delete-cookie [req resp key-name]
-  (let [cookie-name (str config/cookie-prefix key-name)
+        cookie-name (->cookie-name key-name)
         cookies (req :cookies)]
     (if-let [cookie (-> cookies (get cookie-name) :value)]
-      (dissoc cookies cookie-name))
-    (assoc resp :cookies cookies)))
+      (str (cipher/aes-decrypt cookie config/cookie-key)))))
+
+(defn **add-cookies [resp req key-name cookie-entry]
+  (let [cookie-name (->cookie-name (name key-name))
+        cookie {cookie-name cookie-entry}
+        cookies (req :cookies)
+        old-cookie (-> cookies (get cookie-name))]
+    (if (not (nil? old-cookie))
+        (dissoc cookies cookie-name))
+    (let [new-cookies (conj cookies cookie)
+          new-resp (assoc resp :cookies new-cookies)]
+      new-resp)))
+
+(defn add-cookies [resp req key-name value]
+  (let [cookie-entry (gen-cookie value)]
+    (**add-cookies resp req key-name cookie-entry)))
+
+(defn delete-cookie [resp req key-name]
+  (**add-cookies resp req key-name (expired-cookie)))
